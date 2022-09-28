c6222032587c67dfdb41efde6cc1183b00846ad1
Beno Suriano <bjsuriano@gmail.com>
2022-09-28 14:39:20 -0400

    Setup navbar skeleton inside of main app.

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Navbar } from './components'
+
+import { About, Testimonial, Footer, Header, Skills, Work } from './container'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
