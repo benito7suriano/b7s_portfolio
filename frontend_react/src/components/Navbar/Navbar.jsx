295f6c3eda21634ef620456faab00e00529ea51c
Beno Suriano <bjsuriano@gmail.com>
2022-09-29 12:18:22 -0400

    Setup navbar with logo and links.

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -6,10 +6,18 @@ import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className=''>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <img src={images.logo} alt='logo' />
       </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li key={`link-${item}`} className='app__flex p-text'>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
