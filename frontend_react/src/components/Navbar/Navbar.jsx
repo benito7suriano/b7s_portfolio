94e46b7b2fbdd28d4af8ab46420e7aa755d6c7f4
Beno Suriano <bjsuriano@gmail.com>
2022-09-29 12:51:07 -0400

    Style close button for menu.

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Navbar.scss'
 
 import { images } from '../../constants'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -18,6 +21,23 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeInOut' }}>
+            <HiX onClick={() => setToggle(false)} />
+            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              <li key={`item`} className='app__flex p-text'>
+                <a href={`#${item}`} onClick={() => setToggle(false)}>
+                  {item}
+                </a>
+              </li>
+            ))}
+          </motion.div>
+        )}
+      </div>
     </nav>
   )
 }
