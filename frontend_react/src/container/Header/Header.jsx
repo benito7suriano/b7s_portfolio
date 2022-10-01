87b36beace6b970936bb25cdb08dab6f4f1c267a
Beno Suriano <bjsuriano@gmail.com>
2022-09-30 23:02:39 -0400

    Create name section of header.

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
+import { images } from '../../constants'
 import './Header.scss'
 
-const Header = () => {
-  return <div>Header</div>
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
 }
+
+const Header = () => (
+  <div className='app__header app__flex'>
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      className='app__header-info'>
+      <div className='app__header-badge'>
+        <div className='badge-cmp app__flex'>
+          <span>👋</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className='p-text'>Hello, I am</p>
+            <h1 className='head-text'>Beno</h1>
+          </div>
+        </div>
+        <div className='tag-cmp app__flex'>
+          <p className='p-text'>Web Developer</p>
+          <p className='p-text'>Entrepreneur</p>
+        </div>
+      </div>
+    </motion.div>
+  </div>
+)
 
 export default Header
