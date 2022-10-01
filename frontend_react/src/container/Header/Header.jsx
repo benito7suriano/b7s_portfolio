ac9531dcc02f72d531f7511e20ea726937279a6a
Beno Suriano <bjsuriano@gmail.com>
2022-09-30 23:39:18 -0400

    Finish Header section.

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -16,7 +16,7 @@ const scaleVariants = {
 }
 
 const Header = () => (
-  <div className='app__header app__flex'>
+  <div id='home' className='app__header app__flex'>
     <motion.div
       whileInView={{ x: [-100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.5 }}
@@ -34,6 +34,31 @@ const Header = () => (
           <p className='p-text'>Entrepreneur</p>
         </div>
       </div>
+    </motion.div>
+
+    <motion.div
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      className='app__header-img'>
+      <img src={images.profile} alt='profile_bg' />
+      <motion.div
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
+        src={images.circle}
+        alt='profile_circle'
+        className='overlay_circle'
+      />
+    </motion.div>
+
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className='app__header-circles'>
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className='circle-cmp app__flex' key={`circle-${index}`}>
+          <img src={circle} alt='profile_bg' />
+        </div>
+      ))}
     </motion.div>
   </div>
 )
