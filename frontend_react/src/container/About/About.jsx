b0df969eadcc186f74287c7d59fd09232adeb29d
Beno Suriano <bjsuriano@gmail.com>
2022-10-11 22:06:49 -0400

    Update abouts to fetch them from the sanity backend.

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,34 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
+import { urlFor, client } from '../../client'
 
 import { images } from '../../constants'
 
 import './About.scss'
 
-const abouts = [
-  {
-    title: 'Web Development',
-    description: 'I am a good web developer',
-    imgUrl: images.about01,
-  },
-  {
-    title: 'Web Design',
-    description: 'I am a good web designer',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'UI/UX',
-    description: 'I am a good UI/UX designer',
-    imgUrl: images.about03,
-  },
-  {
-    title: 'NERD Stack',
-    description: "I'm a fullstack developer",
-    imgUrl: images.about04,
-  },
-]
-
 const About = () => {
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type === "abouts"]'
+    client.fetch(query).then((data) => setAbouts(data))
+  }, [])
+
   return (
     <>
       <h2 className='head-text'>
@@ -44,7 +29,7 @@ const About = () => {
             transition={{ duration: 0.5, type: 'tween' }}
             className='app__profile-item'
             key={about.title + index}>
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>
               {about.title}
             </h2>
