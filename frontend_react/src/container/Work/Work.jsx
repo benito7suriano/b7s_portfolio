dc7a5b4fe3ef632892b66ef1d2d49b203f8ac651
Beno Suriano <bjsuriano@gmail.com>
2022-10-19 22:57:45 -0400

    Create state inside Work component.

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 
@@ -8,6 +8,11 @@ import { urlFor, client } from '../../client'
 import './Work.scss'
 
 const Work = () => {
+  const [works, setWorks] = useState([])
+  const [filterWork, setFilterWork] = useState([])
+  const [activeFilter, setActiveFilter] = useState('All')
+  const [animatecard, setAnimatecard] = useState({ y: 0, opacity: 1 })
+
   return <div>Work</div>
 }
 
