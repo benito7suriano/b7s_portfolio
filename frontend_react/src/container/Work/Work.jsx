9b00155f08ae2649ba3b9aff554db8489821a544
Beno Suriano <bjsuriano@gmail.com>
2022-10-19 23:02:47 -0400

    Create filter buttons.

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -22,7 +22,30 @@ const Work = () => {
     })
   }, [])
 
-  return <div>Work</div>
+  const handleWorkFilter = () => {}
+
+  return (
+    <>
+      <h2 className='head-text'>
+        My Creative <span>Portfolio</span> Section{' '}
+      </h2>
+
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map(
+          (item, index) => (
+            <div
+              key={index}
+              onClick={() => handleWorkFilter(item)}
+              className={`app__work-filter-item app__flex p-text ${
+                activeFilter === item ? 'item-active' : ''
+              }`}>
+              {item}
+            </div>
+          ),
+        )}
+      </div>
+    </>
+  )
 }
 
 export default AppWrap(Work)
