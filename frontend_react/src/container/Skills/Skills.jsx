d05d52791486c27ec7cc71e263fd8ca7536994d2
Beno Suriano <bjsuriano@gmail.com>
2022-10-27 22:37:37 -0400

    Fetch skills and experiences from sanity.

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -11,7 +11,29 @@ import './Skills.scss'
 const Skills = () => {
   const [experiences, setExperiences] = useState([])
   const [skills, setSkills] = useState([])
-  return <div>Skills</div>
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]'
+    const skillsQuery = '*[_type == "skills"]'
+
+    client.fetch(query).then((data) => {
+      setExperiences(data)
+    })
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data)
+    })
+  }, [])
+
+  return (
+    <>
+      <h2 className='head-text'>Skills & Experiences</h2>
+
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'></motion.div>
+      </div>
+    </>
+  )
 }
 
 export default Skills
