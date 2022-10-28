77d252bfba05b1216499b752e4e58cfb6796335d
Beno Suriano <bjsuriano@gmail.com>
2022-10-27 22:25:37 -0400

    Import react tooltip.

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
+import ReactToolTip from 'react-tooltip'
 
 import { AppWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
