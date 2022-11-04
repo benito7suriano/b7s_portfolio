d6e8c01ed364fa82f59a7cf8263cba2180ef3f38
Beno Suriano <bjsuriano@gmail.com>
2022-11-03 19:12:46 -0600

    Import dependencies to Testimonial and write handleClick function to set index.

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
+import { motion } from 'framer-motion'
+
+import { AppWrap, MotionWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
+import './Testimonial.scss'
 
 const Testimonial = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [testimonials, setTestimonials] = useState([])
+  const [brands, setBrands] = useState([])
+
+  const handleClick = (index) => {
+    setCurrentIndex(index)
+  }
+
   return <div>Testimonial</div>
 }
 
