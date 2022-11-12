9a35bd5173fbebf6a30ad2295b7e021691dc9525
Beno Suriano <bjsuriano@gmail.com>
2022-11-12 17:13:00 -0400

    Create footer form and styling.

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -7,12 +7,94 @@ import './Footer.scss'
 
 const Footer = () => {
   const [formData, setFormData] = useState({ name: '', email: '', message: '' })
-  const [isformSubmitted, setIsformSubmitted] = useState(false)
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false)
   const [loading, setLoading] = useState(false)
 
   const { username, email, message } = formData
 
-  return <div>Footer</div>
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleSubmit = () => {
+    setLoading(true)
+
+    const contact = {
+      _type: 'contact',
+      name: formData.username,
+      email: formData.email,
+      message: formData.message,
+    }
+
+    client
+      .create(contact)
+      .then(() => {
+        setLoading(false)
+        setIsFormSubmitted(true)
+      })
+      .catch((e) => console.error(e))
+  }
+
+  return (
+    <>
+      <h2 className='head-text'>Take a coffee & chat with me</h2>
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.email} alt='email' />
+          <a href='mailto:bjsuriano@gmail.com' className='p-text'>
+            bjsuriano@gmail.com
+          </a>
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt='phone' />
+          <a href='tel:+18097630390' className='p-text'>
+            +1 809 763 0390
+          </a>
+        </div>
+      </div>
+      {!isFormSubmitted ? (
+        <div className='app__footer-form app__flex'>
+          <div className='app__flex'>
+            <input
+              type='text'
+              className='p-text'
+              placeholder='Your Name'
+              name='username'
+              value={username}
+              onChange={() => {}}
+            />
+          </div>
+          <div className='app__flex'>
+            <input
+              type='email'
+              className='p-text'
+              placeholder='Your Email'
+              name='email'
+              value={email}
+              onChange={() => {}}
+            />
+          </div>
+          <div>
+            <textarea
+              className='p-text'
+              placeholder='Your Message'
+              value={message}
+              name='message'
+              onChange={() => {}}
+            />
+          </div>
+          <button type='button' className='p-text' onClick={handleSubmit}>
+            {!loading ? 'Send Message' : 'Sending...'}
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3 className='head-text'>Thank you for getting in touch!</h3>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Footer
