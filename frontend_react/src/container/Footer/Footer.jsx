71394c7d0f57a396a74a317bd886db7fd314982e
Beno Suriano <bjsuriano@gmail.com>
2022-11-12 16:59:00 -0400

    Initialize state for footer form.

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { images } from '../../constants'
+import { AppWrap, MotionWrap } from '../../wrapper'
+import { client } from '../../client'
 import './Footer.scss'
 
 const Footer = () => {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' })
+  const [isformSubmitted, setIsformSubmitted] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const { username, email, message } = formData
+
   return <div>Footer</div>
 }
 
